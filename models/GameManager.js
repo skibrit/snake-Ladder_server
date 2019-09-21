2f267149414d4361395e687a76f5e5ba48b1bcfb
Saidul Amin <Full Stack Web Developer>
2019-09-22 03:24:41 +0600

    updated matchmake function

--- models/GameManager.js
+++ models/GameManager.js
@@ -1,47 +1,52 @@
 const State = require("../models/StateManager");
-const {playerList,lobbyPlayerList,matchList} = State;
+const { playerList, lobbyPlayerList, matchList } = State;
 
-class GameManager{
-    static switchPlayerToBot(player,match){
-        match.Players.filter(item=>item.id==player.ID)[0].isBot = true;
+class GameManager {
+  static switchPlayerToBot(player, match) {
+    match.Players.filter(item => item.id == player.ID)[0].isBot = true;
+  }
+  static UpdatePlayerPosition(player, match, payload) {
+    const { ID } = player;
+    const { Players } = match;
+    const { diceResult, newPos } = payload;
+    match.movePlayer(ID, newPos);
+    match.changeTurn();
+
+    const otherPlayers = Players.filter(item => item.id != ID && !item.isBot);
+    for (const { id } of otherPlayers) {
+      let pl = playerList[id];
+      if (pl && pl.Socket) {
+        const obj = {
+          ...payload,
+          nextTurn: match.WhoseTurn,
+          updateOf: ID,
+          updateIndex: player.MatchIndex
+        };
+        console.log(JSON.stringify(obj));
+        pl.Socket.emit("positionUpdate", obj);
+      }
     }
-    static UpdatePlayerPosition(player,match,payload){
-        const {ID} = player;
-        const {Players} = match;
-        const {diceResult,newPos} = payload;
-        match.movePlayer(ID,newPos);
-        match.changeTurn();
 
-        const otherPlayers = Players.filter(item=>item.id!=ID && !item.isBot);
-        for(const {id} of otherPlayers){
-            let pl = playerList[id];
-            if(pl && pl.Socket){
-                const obj = {...payload,nextTurn:match.WhoseTurn,updateOf:ID,updateIndex:player.MatchIndex};
-                console.log(JSON.stringify(obj));
-                pl.Socket.emit("positionUpdate",obj);
-            }
-        }
-
-        //check if player position is equal to 100 or more
-        if(match.getPlayerPos(ID)>=100){
-            GameManager.declareWinner(player,match);
-        }
+    //check if player position is equal to 100 or more
+    if (match.getPlayerPos(ID) >= 100) {
+      GameManager.declareWinner(player, match);
     }
+  }
 
-    static declareWinner(player,match){
-        const {Players} = match;
-        match.endMatch(player.MatchIndex);
-        const sockets = Players.map(item=>playerList[item.id].Socket);
-        for(const socket of sockets){
-            socket.emit("gameOver",{
-                winner:{id:player.ID,index:player.MatchIndex}
-            })
-        }
-        setTimeout(()=>{
-            //destroy the match object after 30 second
-            delete matchList[match.id];
-        },30*1000);
+  static declareWinner(player, match) {
+    const { Players } = match;
+    match.endMatch(player.MatchIndex);
+    const sockets = Players.map(item => playerList[item.id].Socket);
+    for (const socket of sockets) {
+      socket.emit("gameOver", {
+        winner: { id: player.ID, index: player.MatchIndex }
+      });
     }
+    setTimeout(() => {
+      //destroy the match object after 30 second
+      delete matchList[match.id];
+    }, 30 * 1000);
+  }
 }
 
 module.exports = GameManager;
